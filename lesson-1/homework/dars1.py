15c0ae1b9f57050ac9d2d0c369dd20acf1c5458c
Shahboz Gulomov <77021529+shahbozgulomov@users.noreply.github.com>
2025-04-11 10:56:42 +0100

    Add files via upload

---
+++ lesson-1/homework/dars1.py
@@ -0,0 +1,31 @@
+number1 = 2.0
+number2 = 3
+number4 = number1**number2
+number4
+#Given a side of square. Find its perimeter and area
+side = float(input("the side of the side: "))
+perimeter = side * 4
+area = side * side 
+print(f'the perimeter of the square is {perimeter}')
+print(f'the square of the square is {area}')
+#Given diameter of circle. Find its length.
+diameter = float(input("the diameter of the circle is"))
+length = diameter*3.14
+print(f'the lenhth of the circle is {length}')
+#Given two numbers a and b. Find their mean
+a = float(input("type the value of a:"))
+b = float(input("type the value of b:"))
+mean = (a+b)/2
+print(f'the mean of a and b is {mean}')
+#Given two numbers a and b. Find their sum, product and square of each number.
+a = float(input("the value of a :"))
+b = float(input("the value of b :"))
+sum = a+b
+product = a*b
+square_a = a*a
+square_b = b**2
+print(sum)
+print(product)
+print(square_a)
+print(square_b)
+# bad comment
